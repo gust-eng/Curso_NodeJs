35246c5da3d984007020e1675b35234703248d0e
gust-eng <gustavolana232@gmail.com>
2025-09-09 17:52:06 -0300

    realizado

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('oi serei uma api em breve!!')
+import express from 'express';
+
+const servidor = express();
+
+servidor.get('/helloword' , (req , resp) =>{
+    resp.send('Hello Word !!')
+})
+
+
+servidor.get('/calculadora/somar/:n1/:n2' , (req , resp) =>{
+   let n1 = Number(req.params.n1);
+   let n2 = Number(req.params.n2);
+   let soma = n1 + n2
+   resp.send('A soma é ' + soma)
+})
+
+servidor.get('/calculadora/subtrair/:n1/:n2' , (req , resp) =>{
+   let n1 = Number(req.params.n1);
+   let n2 = Number(req.params.n2);
+   let soma = n1 - n2
+   resp.send('A soma é ' + soma)
+})
+
+servidor.get('/calculadora/dividir/:n1/:n2' , (req , resp) =>{
+   let n1 = Number(req.params.n1);
+   let n2 = Number(req.params.n2);
+   let soma = n1 / n2
+   resp.send('A soma é ' + soma)
+})
+
+servidor.get('/calculadora/multiplicar/:n1/:n2' , (req , resp) =>{
+   let n1 = Number(req.params.n1);
+   let n2 = Number(req.params.n2);
+   let soma = n1 * n2
+   resp.send('A soma é ' + soma)
+})
+
+
+
+servidor.listen(5010 , () => console.log('API subiu com sucesso!!')  )
